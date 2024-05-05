8547a4d5e25f08e96e78eba17c92b5867e9f738d
MarkYoo23 <71996956+MarkYoo23@users.noreply.github.com>
2024-05-05 20:41:25 +0900

    feat : 마지막 커밋

---
+++ Toy.Auth/infrastructures/SampleContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Toy.Auth.Domains.Models;
+
+namespace Toy.Auth.infrastructures
+{
+    public class SampleContext : DbContext
+    {
+        public DbSet<ApplicationJwtConfig> Configs { get; set; }
+
+    }
+
+    public class SampleRunner 
+    {
+        public void Run() 
+        {
+            var context = new SampleContext();
+            var query = context.Configs.Where(row => row.Issuer.Equals(""));
+            var queryString = query.ToQueryString();
+
+
+
+        }
+    }
+}
